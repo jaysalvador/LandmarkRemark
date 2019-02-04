65221c82f17b884c81ac83de88342f21efdfe657
Jay Salvador <jay.andrae.salvador@gmail.com>
2019-02-04 11:08:24 +1100

    Initial Commit

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Jay Salvador on 4/2/19.
+//  Copyright © 2019 Jay Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
